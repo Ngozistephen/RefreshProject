19e62bef168f43c91acc055bfadb6d293b5c0705
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-11-12 03:52:21 +0100

    Added a bootstrap template and worked on Header

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Contact Us</h1>
+                  
+                </div>
+            </div>
+        </header>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-12">
+                    <p class="lead mb-0">Contact Text Here</p>
+                    <!-- Featured blog post-->
+                    {{-- <div class="card mb-4">
+                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                        <div class="card-body">
+                            <div class="small text-muted">January 1, 2023</div>
+                            <h2 class="card-title">Featured Post Title</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                            <a class="btn btn-primary" href="#!">Read more →</a>
+                        </div>
+                    </div> --}}
+           
+                </div>
+                <!-- Side widgets-->
+              
+            </div>
+        </div>
+     
+ @endsection('content')       
